a92ff6b78a550354c16354076b96e4234941de66
ParthBhanti <143245362+ParthBhanti@users.noreply.github.com>
2024-08-12 22:31:12 +0530

    Create Q7_Practicals.cpp

---
+++ Q7_Practicals.cpp
@@ -0,0 +1,16 @@
+//This part includes Parth Bhanti's(23BCE11758) contribution to the code:
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// Structure to hold book details
+struct Book {
+    int id;
+    string author;
+    float price;
+    int pages;
+    string publisher;
+    int year;
+};
+//Parth Bhanti(23BCE11758) has concluded his part.
