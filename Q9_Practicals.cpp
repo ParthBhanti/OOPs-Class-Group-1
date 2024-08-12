4b04f335514a472b5f28f3a6e96d24736b246411
ParthBhanti <143245362+ParthBhanti@users.noreply.github.com>
2024-08-12 23:31:43 +0530

    Create Q9_Practicals.cpp

---
+++ Q9_Practicals.cpp
@@ -0,0 +1,8 @@
+//This part includes Parth Bhanti's(23BCE11758) contribution to the code:
+#include <iostream>
+using namespace std;
+
+// Template function to find the largest of three numbers
+template <typename T>
+
+//Parth Bhanti(23BCE11758) has concluded his part.
