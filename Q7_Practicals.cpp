65e86d21117d4d0f990f360c430780c76226c14e
Taha10981 <syed.23bce10981@vitbhopal.ac.in>
2024-08-12 22:36:09 +0530

    Update Q7_Practicals.cpp

--- Q7_Practicals.cpp
+++ Q7_Practicals.cpp
@@ -14,3 +14,30 @@ struct Book {
     int year;
 };
 //Parth Bhanti(23BCE11758) has concluded his part.
+//This part includes Syed Taha Rizvi's(23BCE10981) contribution to the code:
+// Function to read book details from standard input
+vector<Book> readBooks(int n) {
+    vector<Book> books(n);
+
+    for (int i = 0; i < n; ++i) {
+        cout << "Enter details for book " << i + 1 << ":\n";
+        cout << "Book ID: ";
+        cin >> books[i].id;
+        cin.ignore(); // Ignore the newline after the integer input
+        cout << "Author: ";
+        getline(cin, books[i].author);
+        cout << "Price: ";
+        cin >> books[i].price;
+        cout << "Number of Pages: ";
+        cin >> books[i].pages;
+        cin.ignore(); // Ignore the newline after the integer input
+        cout << "Publisher: ";
+        getline(cin, books[i].publisher);
+        cout << "Year of Publishing: ";
+        cin >> books[i].year;
+        cout << endl;
+    }
+
+    return books;
+}
+//Syed Taha Rizvi(23BCE10981) has concluded his part.
